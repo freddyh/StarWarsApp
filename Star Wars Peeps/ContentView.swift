c150b5676d72ba5d1b96b21b13c61451b4214152
fhj <307890+freddyh@users.noreply.github.com>
2020-08-03 23:51:27 -0700

    Initial Commit

---
+++ Star Wars Peeps/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Star Wars Peeps
+//
+//  Created by Freddy Hernandez Jr on 8/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Star Wars Peeps/Star_Wars_PeepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Star_Wars_PeepsApp.swift
+//  Star Wars Peeps
+//
+//  Created by Freddy Hernandez Jr on 8/3/20.
+//
+
+import SwiftUI
+
+@main
+struct Star_Wars_PeepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
